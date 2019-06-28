633cfd7bceac7cdf926e756ba35170cbb6801a51
Shane Thomas <shane@thirdandgrove.com>
2019-06-28 11:35:50 -0400

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
       resolve: `gatsby-source-drupal-preview`,
       options: {
         baseUrl: `http://dev-codekarate-d8.pantheonsite.io`,
-        //baseUrl: `http://localhost:32790`,
         apiBase: `jsonapi`,
         preview: true
       }
